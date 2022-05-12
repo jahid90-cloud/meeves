d4143ec7abe1d090d1ee36b9b39c7caee7573520
Jahid <jahid90@users.noreply.github.com>
2022-05-12 20:04:17 +0530

    add request logging

--- src/express/index.js
+++ src/express/index.js
@@ -1,12 +1,32 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const morgan = require('morgan');
+const { v4: uuid } = require('uuid');
+
+const primeRequestContext = () => {
+    return (req, res, next) => {
+        req.context = {
+            ...req.context,
+            requestId: uuid(),
+        };
+
+        next();
+    };
+};
 
 const createApp = ({ env, config }) => {
     const app = express();
 
+    // configure morgan custom tokens
+    morgan.token('request-id', (req, res) => {
+        return req.context.requestId;
+    });
+
     // attach middlewares
+    app.use(primeRequestContext());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(morgan(':date[iso] - [:request-id] :method :url :status :res[content-length] - :response-time ms'));
 
     // mount routes
     app.use('/', config.homeApp);
